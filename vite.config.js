1695b18676fe5ed3035709552bb3bdce5a4af884
tal0311 <tal.amit0311@gmail.com>
2024-04-27 23:25:17 +0300

    add base url

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     VueDevTools(),
   ],
+  base: '/grocery-list',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
