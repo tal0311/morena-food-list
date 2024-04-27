c2f758bf99ce796e0a4671039dc80c8ce03a9c47
tal0311 <tal.amit0311@gmail.com>
2024-04-27 23:20:33 +0300

    favicon & title

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,28 @@
 </template>
 
 <script setup>
+import { ref, onBeforeMount } from 'vue';
+
+onBeforeMount(() => {
+  setTiTle()
+})
+
+function setTiTle() {
+  document.title = `Morena ${getIcon()} grocery list`
+
+}
+
+function getIcon() {
+  const icons = ["✅", "🥬", "🥦", "🥒", "🥑", "🫒", "🍅", "🍄", "🍑", "🍌", "🍇", "☕", "🥛", "🥩", "🧀", "🥚", "🥝", "🍉", "🍎", "🥭", "🍍", "🍌", "🍋", "🍊", "🍒", "🍓", "🫐", "🫑", "🌽", "🍆", "🫒", "🍅"];
+  return icons[Math.floor(Math.random() * icons.length)];
+}
+
+
 </script>
 
 <style scoped>
-.main-app{
+.main-app {
   height: 100dvh;
-  
+
 }
 </style>
