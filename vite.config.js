c1b98aed1237cbf7b4fa81eeca539c89a970bd73
tal0311 <tal.amit0311@gmail.com>
2024-04-28 19:56:27 +0300

    base URL

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     VueDevTools(),
   ],
-  // base: '/grocery-list',
+  base: '/grocery-list',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
