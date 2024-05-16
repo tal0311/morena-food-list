f2d2de03dd267639f6ba321d291d6bdec32e3162
tal0311 <tal.amit0311@gmail.com>
2024-05-16 19:56:17 +0300

    vegetables more info

--- src/services/moreinfo.service.js
+++ src/services/moreinfo.service.js
@@ -1,15 +1,52 @@
 const info = {
-    'vegetables': {
+    'vegetable': {
         he: {
-            info: `ירקות הם מקור עשיר בויטמינים, מים וסיבים. הם יכולים להשתלב בכל סוגי המאכלים ולהעשיר אותם בטעם ובצבע.`,
+            info: `
+            תכולת הפחמימות בהם (עמילן) מתחלקת לעתירים ודלים.
+            חשוב להבין ששניהם מעולים לצריכה ע”ב יומי ורק את היחסים שלהם על הצלחת צריך להביi.\n
+            ירקות לא עמילניים:\n
+            לרוב יגיעו בגוונים ירקרקים כגון: מלפפון, כרוב, תרד, כוסברה, פטרוזיליה, שעועית ירוקה ועוד.
+            מה שכל כך טוב בהם הוא כמות העמילן העמיד = העמילן שלא מתעכל ותורם לעיכול, לשובע, לנוחות במעיים ובגוף של עצמנו. למחשבה בהירה, לריכוז, מיקוד וקלילות פיזית ומחשבתית.
+            מעולים תמיד, תמיד כתוספת בצלחת בכל ארוחה עיקרית כדי לשמור על תחושת ערנות אחרי הארוחה ומומלץ להוסיף אותם לחיזוק הטעם המלוח והאוממי בתבשילים.
+            ירקות עמילניים:\n
+            יגיעו במגוון צבעים: לבנים, צהבהבים, כתומים, אדומים וסגולים. העתירים בעמילן שביניהם יהיו הבטטה ותפו”א והם מעולים כתוספת קטנה לצד מנת חלבון עיקרית ושומן.
+            לא מומלץ לאכול אותם כמנה עיקרית בשל העובדה שלא מחזיקים שובע ובשל כך עשויים להיאכל בכמות גדולה שתשאיר אי נוחות במעיים ובגוף הפיזי, דבר שעלול להוביל לירידה בדרייב ובתחושת המסוגלות הרגשית.
+            ירקות כמו: סלק, גזר ובצל מכילים גם הם עמילנים אך בכמות פחותה יותר והם מצוינים כתוספת לצד ארוחה עיקרית או בהעשרת תבשילים, מרקים וסלטים.
+            ירקות אלו יכולים להיחשב על הצלחת כפחמימה במנה בשל עושרם הפחממתי והיכולת שלהם לספק את האנרגיה המומלצת מרכיב תזונה זה.
+            הם ממריצים את המטבוליזם ולרוב משאירים אחריהם תחושת חיוניות ומרץ.
+            אופן ההכנה המומלץ שלהם יהיה בבישול, הקפצה, אפייה, צלייה ואת חלקם גם בגרסה הטרייה.`,
             title: 'ירקות'
         },
         en: {
-            info: `Vegetables are a rich source of vitamins, water and fiber. They can be integrated into all types of foods and enrich them with taste and color.`,
+            info: `The carbohydrate content in them (starch) is divided into rich and poor.
+            It is important to understand that both are excellent for daily consumption, and only their proportions on the plate need to be understood.
+            Non-starchy vegetables:
+            Most will come in green shades such as cucumber, cabbage, celery, zucchini, parsley, green beans, and more.
+            What's so good about them is the amount of resistant starch = starch that is not digested and contributes to digestion, satiety, comfort in the intestines and our body. For clear thinking, concentration, focus, and physical and mental lightness.
+            They are always excellent, always as an addition to the plate in every main meal to maintain alertness after the meal and it is recommended to add them to enhance the salty and umami taste in dishes.
+            Starchy vegetables:
+            They come in a variety of colors: white, golden, orange, red, and purple. The rich in starch among them will be potatoes and sweet potatoes, and they are excellent as a small addition alongside the main protein and fat portion.
+            It is not recommended to eat them as a main course because they do not provide satiety and therefore may be eaten in a large amount that will cause discomfort in the intestines and the physical body, which may lead to a decrease in drive and emotional capability.
+            Vegetables like beets, carrots, and onions also contain starch, but in a lesser amount, and they are excellent as an addition alongside a main meal or to enrich dishes, soups, and salads.
+            These vegetables can be considered as a carbohydrate portion per serving due to their carbohydrate richness and their ability to provide the recommended energy from this nutritional component.
+            They boost metabolism and usually leave behind a feeling of vitality and vigor.
+            The recommended preparation method for them is cooking, steaming, baking, grilling, and some of them also in their raw version.`,
             title: 'Vegetables'
         },
         es: {
-            info: `Las verduras son una fuente rica de vitaminas, agua y fibra. Se pueden integrar en todo tipo de alimentos y enriquecerlos con sabor y color.`,
+            info: `El contenido de carbohidratos en ellos (almidón) se divide en ricos y pobres.
+            Es importante entender que ambos son excelentes para el consumo diario, y solo se debe entender sus proporciones en el plato.
+            Vegetales no almidonados:
+            La mayoría vendrá en tonos verdes como pepino, col, apio, calabacín, perejil, judías verdes y más.
+            Lo bueno de ellos es la cantidad de almidón resistente = almidón que no se digiere y contribuye a la digestión, saciedad, comodidad en los intestinos y nuestro cuerpo. Para un pensamiento claro, concentración, enfoque y ligereza física y mental.
+            Siempre son excelentes, siempre como adición al plato en cada comida principal para mantener la vigilancia después de la comida y se recomienda agregarlos para realzar el sabor salado y umami en los platos.
+            Vegetales almidonados:
+            Vienen en una variedad de colores: blanco, dorado, naranja, rojo y morado. Los ricos en almidón entre ellos serán las papas y batatas, y son excelentes como una pequeña adición junto a la porción principal de proteínas y grasas.
+            No se recomienda comerlos como plato principal porque no proporcionan saciedad y, por lo tanto, pueden ser consumidos en una gran cantidad que causará molestias en los intestinos y el cuerpo físico, lo que puede llevar a una disminución en el impulso y la capacidad emocional.
+            Vegetales como remolachas, zanahorias y cebollas también contienen almidón, pero en menor cantidad, y son excelentes como adición junto a una comida principal o para enriquecer platos, sopas y ensaladas.
+            Estos vegetales pueden considerarse como una porción de carbohidratos por porción debido a su riqueza en carbohidratos y su capacidad para proporcionar la energía recomendada de este componente nutricional.
+            Aumentan el metabolismo y generalmente dejan atrás una sensación de vitalidad y vigor.
+            El método de preparación recomendado para ellos es cocinar, al vapor, hornear, asar a la parrilla, y algunos de ellos también en su versión cruda.`,
             title: 'Verduras'
         }
     },
