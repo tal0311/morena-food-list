64b67fee234fbc8e1f6988324f1272a7e07193ff
tal0311 <tal.amit0311@gmail.com>
2024-08-03 17:41:22 +0300

    Staging env and scripts

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import VueDevTools from 'vite-plugin-vue-devtools'
-
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import VueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     vue(),
     VueDevTools(),
   ],
-  
+  base: mode === 'staging' ? '/morena-food-list' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
- 
-  
-})
+  }
+}));
