51a5273394d2be510899cc2530946a248d772e85
tal0311 <tal.amit0311@gmail.com>
2024-08-03 20:23:02 +0300

    process.env

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => ({
     vue(),
     VueDevTools(),
   ],
+  'process.env': process.env,
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
