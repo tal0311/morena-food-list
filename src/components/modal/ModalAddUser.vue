950489a194a5dbd8aea44bba4d6fad964483c8a6
tal0311 <tal.amit0311@gmail.com>
2024-10-07 15:14:29 +0300

    Refactor to single entity table

--- src/components/modal/ModalAddUser.vue
+++ src/components/modal/ModalAddUser.vue
@@ -1,27 +1,52 @@
 <template>
 
-    <section class="modal-add-user">
+    <section v-if="user" class="modal-add-user">
 
 
-        <form @submit.prevent="addUser" class="grid grid-dir-cols">
+        <form @submit.prevent="" class="grid grid-dir-cols">
 
-            <label for="" class="grid grid-dir-cols">
-                username
-                <input type="text" v-model="user.name" placeholder="name" />
+            <label  class="grid grid-dir-cols">
+                Username
+                <input type="text" v-model="user.username" placeholder="name" required />
             </label>
 
-            <label for="" class="grid grid-dir-cols">
-                email
-                <input type="text" v-model="user.email" placeholder="email" />
+            <label  class="grid grid-dir-cols">
+                Email <small>(changing this will require the user to re-login)</small>
+                <input type="text" v-model="user.email" placeholder="email" disabled required />
             </label>
 
-            <label for="" class="grid grid-dir-cols">
-                role
-                <input type="text" v-model="user.role" placeholder="role" />
+            <label  class="grid grid-dir-cols">
+                Role <small>(this will set permissions for the user)</small>
+                <input type="text" v-model="user.role" placeholder="role" required />
 
             </label>
 
-            <button class="secondary-btn" type="submit">add</button>
+            <label  class="grid grid-dir-cols">
+                Password <small>(leave empty to keep the same)</small>
+                <input :type="`${!isPasswordVisible? 'password' :'text'}`" v-model="user.password" placeholder="Can't see other users password"  />
+                <button @click="displayPasswd">See Password</button>
+            </label>
+
+            <div>
+                <label >
+                    Excluded items
+                    <input type="text" class="items-input" list="list-items" v-model="selectedItem"
+                    placeholder="Select item">
+                    <button class="primary-btn" @click="AddItem">Add</button>
+                    
+                    <ul v-if="user.exItems" class="small-item-list">
+                        <li v-for="item in user.exItems" class="grid grid-dir-col">
+                            <span>{{ item.name }}</span>
+                            <button class="icon-svg" type="button" @click.stop="removeFromList(item._id)"
+                            v-html="$svg('close')"></button>
+                        </li>
+                    </ul>
+                </label>
+            </div>
+
+
+
+            <button class="secondary-btn" type="submit" @click="saveUser">{{ user._id ? 'Update' : 'Add' }}</button>
 
         </form>
 
@@ -29,16 +54,71 @@
 
 
     <section>
+        <datalist id="list-items">
+            <div v-for="item in items" :key="item._id">
+                <option v-if="!isItemInList(item.name)" :value="item.name"></option>
+            </div>
 
-        <!-- {{ user }} -->
+        </datalist>
     </section>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, onBeforeMount } from 'vue'
 import { userService } from '@/services/user.service';
+import { itemService } from '@/services/item.service';
+import { eventBus } from '@/services/event-bus.service';
+const props = defineProps(['info'])
+const emit = defineEmits(['resetModal'])
 
-const user = ref(userService.getEmptyUser())
+const selectedItem = ref('')
+const user = ref(null)
+const items = ref(null)
+onBeforeMount(async () => {
+    items.value = await itemService.query()
+    if (props.info) {
+        props.info.exItems = items.value.filter(item => props.info.exItems.includes(item._id))
+        user.value = props.info
+    } else {
+        user.value = userService.getEmptyUser()
+    }
+
+})
+
+function isItemInList(itemName) {
+    const isItemInList = user.value.exItems.find(item => item.name === itemName)
+    return isItemInList ? true : false
+}
+
+function AddItem() {
+    const item = items.value.find(item => item.name === selectedItem.value)
+    if (!item) return
+    user.value.exItems.push(item)
+    selectedItem.value = ''
+}
+
+function removeFromList(itemId) {
+    user.value.exItems = user.value.exItems.filter(i => i._id !== itemId)
+}
+
+async function saveUser() {
+    const userToSave = JSON.parse(JSON.stringify(user.value))
+    userToSave.exItems = userToSave.exItems.map(item => item._id)
+    await userService.save(userToSave)
+    if (userToSave._id){
+        eventBus.emit('user-updated', userToSave)
+    }else{
+        eventBus.emit('user-added', userToSave)
+    }
+    
+    emit('resetModal')
+}
+
+const isPasswordVisible = ref(false)
+function displayPasswd(){
+    isPasswordVisible.value = !isPasswordVisible.value
+  
+}
 </script>
 
 <style scoped>
@@ -57,24 +137,53 @@ const user = ref(userService.getEmptyUser())
 
             font-size: 1.2rem;
             gap: 0.5rem;
+
             input {
-                color:var(--bClr3) ;
+                color: var(--bClr3);
                 padding: 0.2rem 0.5rem;
                 border-radius: var(--br);
                 font-size: inherit;
-                border:1px solid var(--bClr3);
+                border: 1px solid var(--bClr3);
 
-                &::placeholder{
-                    color:var(--bClr1) ;
+                &::placeholder {
+                    color: var(--bClr1);
                 }
             }
 
 
         }
-        .secondary-btn{
+
+        .secondary-btn {
             max-width: 100px;
             padding: 0.5rem 1rem;
         }
     }
+}
+
+.small-item-list {
+    display: grid;
+    gap: 1rem;
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+    padding: 0;
+
+    li {
+        align-items: center;
+        background-color: var(--bClr1);
+        grid-template-columns: 1fr auto;
+        padding: 0.3rem 0.5rem;
+        border-radius: var(--br);
+
+        .icon-svg {
+            border: none;
+            /* background-color: var(--bClr4); */
+            border-radius: 50%;
+            padding: 0.2rem;
+        }
+    }
+}
+
+.primary-btn {
+    padding: 0.5rem;
+    max-width: max-content;
 }
 </style>
--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -2,201 +2,11 @@
     <section v-if="users || items || lists || recipes" :class="['admin-view', filterBy]">
 
         <h1>Welcome Moran, You can update your data base from here</h1>
-        <section class="admin-filter grid ">
+        <DashboardFilter :filterBy="filterBy" v-model="searchTerm" @setFilterBy="setFilterBy" />
 
-            <section class="entity-filter grid">
-                <span>Filter By: </span>
-
-                <section class="actions-container grid grid-dir-col">
-
-                    <button class="primary-btn" @click="setFilterBy('users')">Users</button>
-                    <button class="primary-btn" @click="setFilterBy('items')">Items</button>
-                    <button class="primary-btn" @click="setFilterBy('lists')">Lists</button>
-                    <button class="primary-btn" @click="setFilterBy('recipes')">Recipes</button>
-                </section>
-                <!-- <button class="secondary-btn" @click="addUser">Create user</button> -->
-            </section>
-            <form action="">
-                Search: <input type="text" v-model="searchTerm" :placeholder="`Search ${filterBy}`" />
-
-            </form>
-        </section>
-
-        <details :open="filterBy === 'users'">
-            <summary>Users</summary>
-            <div class="create-btn grid">
-                <button @click="createUser">create</button>
-            </div>
-
-            <table>
-                <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Role</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="user in filteredUsers" :key="user.id">
-                        <td>{{ user._id }}</td>
-                        <td contenteditable>{{ user.name }}</td>
-                        <td contenteditable>{{ user.email }}</td>
-                        <td contenteditable>{{ user.role }}</td>
-                        <td>
-                            <button @click="deleteUser(user._id)">Delete</button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </details>
-
-        <details :open="filterBy === 'items'">
-            <summary>Food Items</summary>
-
-            <div class="create-btn grid">
-                <button @click="addItem">create</button>
-            </div>
-
-
-
-
-            <table>
-                <thead>
-
-                    <tr>
-                        <th v-if="false">Id</th>
-                        <th>Group</th>
-                        <th>Name</th>
-                        <th v-if="false">Color</th>
-                        <th v-if="false">Read More URL</th>
-                        <th v-if="false">Icon</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="{ _id, group, color, readMoreURL, icon, name } in filteredItems" :key="_id"
-                        :class="`item item-${_id}`">
-                        <td v-if="false">{{ _id }}</td>
-                        <td>
-                            <span class="text-bold">
-                                {{ group }}
-                            </span>
-                            <ul class="clean-list grid">
-                                <li v-for="value, key in getTranslation(group)">
-                                    <small>
-                                        {{ key }}: {{ value }}
-                                    </small>
-                                </li>
-                            </ul>
-                        </td>
-                        <td>
-                            <span class="text-bold">{{ name }}</span>
-                            <ul class="clean-list grid">
-                                <li v-for="value, key in getTranslation(name)">
-                                    <small>
-                                        {{ key }}: {{ value }}
-                                    </small>
-                                </li>
-                            </ul>
-                        </td>
-                        <td v-if="false">
-                            {{ color }}
-                        </td>
-                        <td v-if="false">
-                            {{ readMoreURL || 'No Read More Url' }}
-                        </td>
-                        <td v-if="false">{{ icon }}</td>
-                        <td>
-                            <div class="actions-container grid grid-dir-col">
-
-                                <button @click="selectItem(_id)">Select</button>
-                                <button @click="removeItem(_id)">Delete</button>
-                            </div>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </details>
-
-
-        <details :open="filterBy === 'recipes'">
-            <summary>Recipes</summary>
-            <div class="create-btn grid">
-                <button @click="createRecipe">create</button>
-            </div>
-            <table>
-                <thead>
-                    <tr>
-                        <th v-if="false">Id</th>
-                        <th>Title</th>
-                        <th>Group</th>
-                        <th>Image</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="recipe in filteredRecipes" :key="recipe._id" class="recipe">
-                        <td v-if="false">{{ recipes._id }}</td>
-                        <td>{{ recipe.he.title }}
-                            <hr>
-                            {{ recipe.en.title }}
-                        </td>
-
-                        <td>
-                            {{ recipe.he.description }}
-                            <hr>
-                            {{ recipe.en.description }}
-                        </td>
-
-                        <td>{{ recipe.imgUrl ? '✅' : '❌' }}</td>
-                        <td>
-                            <div class="actions-container grid grid-dir-col">
-
-                                <button @click="removeRecipe(recipe._id)">Delete</button>
-                                <button @click="selectRecipe(recipe._id)">Select</button>
-                            </div>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </details>
-
-        <details :open="filterBy === 'lists'">
-            <summary>Lists</summary>
-            <div class="create-btn grid">
-                <button @click="createList">create</button>
-            </div>
-            <table>
-                <thead>
-                    <tr>
-                        <th v-if="false">Id</th>
-                        <th>Title</th>
-                        <th>Items</th>
-                        <th>Owner</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="list in filteredLists" :key="list._id" class="list">
-                        <td v-if="false">{{ list._id }}</td>
-                        <td>{{ list.title }}</td>
-                        <td>{{ list.items.map(item => item.name).join(', ') }}</td>
-                        <td>{{ list.owner.username }}</td>
-                        <td>
-                            <div class="actions-container grid grid-dir-col">
-
-                                <button @click="removeList(list._id)">Delete</button>
-                                <button @click="selectList(list._id)">Select</button>
-                            </div>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </details>
-
-
+        <EntityTable v-for="e in entities" :key="e.name" :filterBy="e.name" :open="isOpen(e.name)"
+            :entity="e.entity" @create="() => createEntity(e.name)" @remove="(id) => removeEntity(e.name, id)"
+            @select="id => selectEntity(e.name, id)" />
     </section>
 
     <section v-else>
@@ -213,6 +23,8 @@ import { listService } from '@/services/list.service';
 import { i18Service } from '@/services/i18n.service';
 import { eventBus } from '@/services/event-bus.service';
 import { recipeService } from '@/services/recipe.service.local';
+import DashboardFilter from '@/components/dashboardCmps/DashboardFilter.vue';
+import EntityTable from '@/components/dashboardCmps/EntityTable.vue';
 
 
 
@@ -220,6 +32,7 @@ const users = ref(null);
 const items = ref(null);
 const lists = ref(null);
 const recipes = ref(null);
+
 
 const searchTerm = ref('');
 const filterBy = ref('');
@@ -231,6 +44,10 @@ const filteredUsers = computed(() => {
     });
 });
 
+
+function isOpen(entity) {
+    return filterBy.value === entity;
+}
 const filteredRecipes = computed(() => {
     return recipes.value?.filter(recipe => {
         const regex = new RegExp(searchTerm.value, 'i');
@@ -253,6 +70,25 @@ const filteredLists = computed(() => {
     });
 });
 
+const entities = [{
+    name: 'user',
+    entity: filteredUsers
+
+}
+    , {
+    name: 'item',
+    entity: filteredItems
+},
+{
+    name: 'list',
+    entity: filteredLists
+},
+{
+    name: 'recipe',
+    entity: filteredRecipes
+}];
+
+
 
 
 const subscriptions = [];
@@ -262,11 +98,15 @@ onBeforeMount(async () => {
     await loadLists();
     await loadUsers();
     await loadRecipes();
+
+    // addListeners();
     subscriptions[0] = eventBus.on('modal-filter', onModalFilter);
     subscriptions[1] = eventBus.on('get-groups-from-admin', getGroups);
     subscriptions[2] = eventBus.on('item-added', loadItems);
-    subscriptions[3] = eventBus.on('item-updated', addUpdatedItem);
-    subscriptions[4] = eventBus.on('list-added', addList);
+    subscriptions[3] = eventBus.on('item-updated', updateItem);
+    subscriptions[4] = eventBus.on('list-added', loadLists);
+    subscriptions[5] = eventBus.on('user-updated', loadUsers);
+    subscriptions[6] = eventBus.on('user-added', loadUsers);
     document.body.dir = 'ltr';
 });
 
@@ -276,41 +116,86 @@ async function loadUsers() {
 
 async function loadLists() {
     lists.value = await listService.query({ admin: true });
-    // console.log('lists', lists.value);
-
 }
 
 async function loadItems() {
     items.value = await itemService.query();
-
 }
 
 async function loadRecipes() {
     recipes.value = await recipeService.query({ admin: true });
 }
 
-function removeRecipe(recipeId) {
+function createEntity(entity) {
+    let modalTYpe = null
+    switch (entity) {
+        case 'user':
+            modalTYpe = 'ModalAddUser'
+            break;
+        case 'item':
+            modalTYpe = 'ModalAddItem'
+            break;
+        case 'list':
+            modalTYpe = 'ModalAddList'
+            break;
+        case 'recipe':
+            modalTYpe = 'ModalAddRecipe'
+            break;
+    }
+    eventBus.emit('toggle-modal', { type: modalTYpe });
+}
 
-    const isConfirm = confirm('Are you sure you want to delete this recipe?');
+function removeEntity(entity, id) {
+    console.log('removing', entity, id);
+
+    const isConfirm = confirm(`Are you sure you want to delete this ${entity}?`);
     if (!isConfirm) return;
-    recipes.value = recipes.value.filter(recipe => recipe._id !== recipeId);
-    // recipeService.remove(recipeId);
+    switch (entity) {
+        case 'user':
+
+            users.value = users.value.filter(user => user._id !== id);
+            userService.remove(id);
+
+            break;
+        case 'item':
+
+            items.value = items.value.filter(item => item._id !== id);
+            itemService.remove(id);
+            break;
+        case 'list':
+
+            lists.value = lists.value.filter(list => list._id !== id);
+            listService.remove(id);
+            break;
+        case 'recipe':
+            recipes.value = recipes.value.filter(recipe => recipe._id !== id);
+            recipeService.remove(id);
+            break;
+    }
 }
 
-function createRecipe() {
-    // eventBus.emit('toggle-modal', { type: 'ModalAddRecipe' });
-    console.log('creating recipe');
+function selectEntity(entity, id) {
+    const modalData = { type: '', info: {} }
+    switch (entity) {
+        case 'users':
+            modalData.type = 'ModalAddUser';
+            modalData.info = users.value.find(user => user._id === id);
 
-}
-
-function selectRecipe(recipeId) {
-    const recipe = recipes.value.find(recipe => recipe._id === recipeId);
-    console.log('selecting', recipe);
-    // eventBus.emit('toggle-modal', { type: 'ModalAddRecipe', info: JSON.parse(JSON.stringify(recipe)) });
-}
-
-async function addUpdatedItem() {
-    await loadItems();
+            break;
+        case 'items':
+            modalData.type = 'ModalAddItem';
+            modalData.info = items.value.find(item => item._id === id);
+            break;
+        case 'lists':
+            modalData.type = 'ModalAddList';
+            modalData.info = lists.value.find(list => list._id === id);
+            break;
+        case 'recipes':
+            modalData.type = 'ModalAddRecipe';
+            modalData.info = recipes.value.find(recipe => recipe._id === id);
+            break;
+    }
+    eventBus.emit('toggle-modal', modalData);
 }
 
 
@@ -321,82 +206,21 @@ function getGroups() {
 }
 
 function onModalFilter(filter) {
-    console.log('filter', filter);
     searchTerm.value = filter;
 }
-
-
 
 function setFilterBy(filter) {
     filterBy.value = filter;
 
 }
 
+function updateItem(item) {
+    items.value = items.value.map(i => i._id === item._id ? item : i);
 
-function saveUser(userId) {
-    const user = users.value.find(user => user._id === userId);
-    console.log('saving', user);
-    // userService.save(user);
-}
-
-function deleteUser(userId) {
-    console.log('deleting', userId);
-    // userService.remove(userId);
-}
-
-function selectItem(itemId) {
-    const item = items.value.find(item => item._id === itemId);
-    eventBus.emit('toggle-modal', { type: 'ModalAddItem', info: JSON.parse(JSON.stringify(item)) });
-    // itemService.save(item);
-}
-
-async function removeItem(itemId) {
-    const isConfirm = confirm('Are you sure you want to delete this item?');
-    if (!isConfirm) return;
-    await itemService.remove(itemId);
-    items.value = items.value.filter(item => item._id !== itemId);
-    console.log('deleted', itemId);
-
-}
-
-
-async function removeList(listId) {
-    const isConfirm = confirm('Are you sure you want to delete this list?');
-    if (!isConfirm) return;
-    try {
-        lists.value = lists.value.filter(list => list._id !== listId);
-        await listService.remove(listId);
-    } catch (error) {
-        await loadLists();
-
-    }
-}
-
-function createList() {
-    eventBus.emit('toggle-modal', { type: 'ModalAddList' });
-}
-
-function selectList(listId) {
-    const list = lists.value.find(list => list._id === listId);
-    eventBus.emit('toggle-modal', { type: 'ModalAddList', info: JSON.parse(JSON.stringify(list)) });
-    // listService.save(list);
-}
-async function addList() {
-    await loadLists();
 }
 
 function getTranslation(key) {
     return i18Service.getTransItem(key);
-}
-
-function createUser() {
-    eventBus.emit('toggle-modal', { type: 'ModalAddUser' })
-}
-function addItem() {
-    // userService.add();
-    eventBus.emit('toggle-modal', { type: 'ModalAddItem' })
-    // console.log(eventBus);
-
 }
 
 onBeforeUnmount(() => {
@@ -408,129 +232,21 @@ onBeforeUnmount(() => {
 <style scoped>
 .admin-view {
 
+    h1{
+        padding-inline: 0.5rem;
+    }
     overflow-x: auto;
 
     align-self: self-start;
     height: 100%;
     width: 100%;
-    background: aliceblue;
+    background: var(--bClr1);
 
     &.items {
         height: fit-content;
     }
 
-    details {
-
-        overflow-x: auto;
-        margin: 1rem;
-        padding: 1rem;
-        background: white;
-        border: 1px solid #ccc;
-        border-radius: 5px;
-
-        table {
-            margin-top: 2rem;
-            width: 100%;
-            border-collapse: collapse;
-
-            th,
-            td {
-                border: 1px solid #ccc;
-                padding: 0.5rem;
-            }
-
-            th {
-                background: #f0f0f0;
-            }
-
-            td {
-                text-align: center;
-            }
-
-            tr:nth-child(even) {
-                background: #f0f0f0;
-            }
-
-            tr:hover {
-                background: #f0f0f0;
-            }
-        }
-    }
+  
 }
 
-.admin-filter {
-
-    gap: 1.5rem;
-
-    .entity-filter {
-        padding-inline: 0.5rem;
-        grid-template-columns: 100px min-content;
-        grid-auto-flow: column;
-        gap: 1rem;
-        align-items: center;
-        font-size: larger;
-
-
-        .secondary-btn {
-            padding: 0.5rem 1rem;
-            font-size: 0.8rem;
-        }
-
-        .actions-container {
-            gap: 1rem;
-
-            button {
-                padding: 0.5rem 1rem;
-
-
-            }
-        }
-    }
-
-    form {
-        display: grid;
-        grid-auto-flow: column;
-        grid-template-columns: min-content auto;
-        background: #f0f0f0;
-        padding: 0.5rem;
-        align-items: center;
-        gap: 0.5rem;
-        font-size: larger;
-
-        input {
-            padding: 0.5rem;
-            border: none;
-            border-radius: 5px;
-            font-size: inherit;
-        }
-
-        button {
-            padding: 0.5rem;
-            font-size: inherit;
-
-        }
-
-
-    }
-}
-
-.create-btn {
-    justify-content: end;
-
-    button {
-        padding: 0.5rem 1rem;
-        font-size: 0.8rem;
-    }
-}
-
-:is(.item, .list, .recipe) {
-    td>.actions-container {
-        gap: 1rem;
-        place-content: center;
-
-        button {
-            padding: 0.5rem 1rem;
-        }
-    }
-}
 </style>
