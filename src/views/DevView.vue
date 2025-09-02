13b1a45e82f5dc8ea995c950a52e2ce7e6a6c3d3
tal0311 <tal.amit0311@gmail.com>
2025-09-02 12:25:35 +0300

    dev view

---
+++ src/views/DevView.vue
@@ -0,0 +1,21 @@
+<template>
+    <section>
+        <h1>Dev View</h1>
+
+
+        <h2>Local Storage</h2>
+        <pre>{{ localStorage }}</pre>
+
+
+
+    </section>
+
+
+</template>
+
+<script setup>
+
+
+</script>
+
+<style scoped></style>
